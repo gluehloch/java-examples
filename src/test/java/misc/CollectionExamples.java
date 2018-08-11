a3495df6ca8d552f330539d208f7afff567747e9
andrewinkler <andre.winkler@web.de>
2018-08-11 08:38:55 +0200

    UPDATE: Some Java 9/10 collection examples.

--- src/test/java/misc/CollectionExamples.java
+++ src/test/java/misc/CollectionExamples.java
@@ -1,6 +1,13 @@
 package misc;
 
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
 
 /**
  * Some new Java 9 / 10 features.
@@ -10,9 +17,46 @@ import java.util.List;
  */
 public class CollectionExamples {
 
-    public void examples() {
-        var integers2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        integers2.sort((a, b) -> a.compareTo(b));
-    }
+	@Test
+	public void createCollections() {
+		// #of() wurde mit Java 9 eingefuehrt
+		var integers2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		System.out.println(integers2);
+
+		var integers3 = Set.of(1, 2, 3, 5, 6, 7, 8);
+		System.out.println(integers3);
+
+		// Map
+		Map<String, Integer> map = Map.of("a", 1, "b", 2, "c", 3);
+		var map2 = Map.of("a", 1, "b", 2, "c", 3);
+		System.out.println(map);
+		System.out.println(map2);
+
+		Map<Integer, String> map3 = Map.ofEntries(entry(1, "Andre"), entry(2, "Lars"));
+		System.out.println(map3);
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void sortCollections() {
+		var integers2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+		// Sortierung einer unveraenderlichen List fuehrt zu einer ...
+		integers2.sort((a, b) -> a.compareTo(b));
+	}
+
+	/**
+	 * Vergleiche mit {@link #sortCollections()}
+	 */
+	@Test
+	public void sortCollectionsAndAssertJ() {
+		var integers2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+		assertThatExceptionOfType(UnsupportedOperationException.class)
+				.isThrownBy(() -> integers2.sort((a, b) -> a.compareTo(b)));
+
+//		assertThatExceptionOfType(UnsupportedOperationException.class)
+//				.isThrownBy(() -> integers2.sort((a, b) -> a.compareTo(b)))
+//				.withMessage("%s", "kaputt");
+	}
 
 }
