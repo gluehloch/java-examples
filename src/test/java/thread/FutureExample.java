c17480c9c00c3b8a0b8b50a34bebf8a42a8ec31b
andrewinkler <andre.winkler@web.de>
2018-10-20 00:44:20 +0200

    CancellationException.

--- src/test/java/thread/FutureExample.java
+++ src/test/java/thread/FutureExample.java
@@ -1,8 +1,11 @@
 package thread;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
@@ -14,7 +17,8 @@ public class FutureExample {
      * Create a future.
      * 
      * @return Future<String> A future
-     * @throws InterruptedException ...
+     * @throws InterruptedException
+     *             ...
      */
     private Future<String> calculateAsync() throws InterruptedException {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
@@ -29,10 +33,12 @@ public class FutureExample {
     }
 
     /**
-     * Create a future with the static helper method.
+     * Create a future with the static helper method. The future is already
+     * completed. {@code #get()} will not wait on the result.
      * 
      * @return Future<String> A future
-     * @throws InterruptedException ...
+     * @throws InterruptedException
+     *             ...
      */
     private Future<String> calculateAsync2() {
         return CompletableFuture.completedFuture("Hello2");
@@ -43,8 +49,36 @@ public class FutureExample {
         Future<String> future = calculateAsync();
         String result = future.get();
         assertThat(result).isEqualTo("Hello");
-        
+
         assertThat(calculateAsync2().get()).isEqualTo("Hello2");
+    }
+
+    private Future<String> calculateAsyncWithCancellation()
+            throws InterruptedException {
+
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(500);
+            completableFuture.cancel(false);
+            return null;
+        });
+
+        return completableFuture;
+    }
+
+    @Test
+    public void cancelCompletableFuture()
+            throws InterruptedException, ExecutionException {
+
+        Future<String> future = calculateAsyncWithCancellation();
+
+        try {
+            future.get(); // CancellationException
+            fail("Expected CancellationException");
+        } catch (CancellationException ex) {
+            // ok
+        }
     }
 
 }
