1701e67dcabe42adfa654fcd9eef0d315096cb11
Andre Winkler <gluehloch@googlemail.com>
2020-04-27 07:00:14 +0200

    Switch-Case with String.

--- src/test/java/java11/Java11FeatureShow.java
+++ src/test/java/java11/Java11FeatureShow.java
@@ -1,5 +1,9 @@
 package java11;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Show me some Java 11 features!
  *
@@ -7,8 +11,16 @@ package java11;
  */
 public class Java11FeatureShow {
 
+    @Test
     public void xxx() {
+        var xxx = "Ahorn";
         // Ups. My Eclipse installation does not suppot JDK 11.
+        switch (xxx) {
+            case "Ahorn": xxx = "Ahorn"; break;
+            case "Buche": xxx = "Buche"; break;
+            default: xxx = "Wiese";
+        }
+        assertThat(xxx).isEqualTo("Ahorn");
     }
 
 }
