3558dce9b1fdaea1012f13096b3e26c0e717df72
Andre Winkler <gluehloch@googlemail.com>
2020-12-30 12:29:54 +0100

    Innerclass access outer class.

---
+++ src/test/java/innerclass/AccessOuterClass.java
@@ -0,0 +1,25 @@
+package innerclass;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AccessOuterClass {
+
+    private String text;
+    
+    public class InnerClass {
+        public String access() {
+            String outerText = AccessOuterClass.this.text;
+            return outerText;
+        }
+    }
+    
+    @Test
+    public void testAccess() {
+        text = "Das ist ein Test";
+        InnerClass innerClass = new InnerClass();
+        assertThat(innerClass.access()).isEqualTo(text);
+    }
+    
+}
