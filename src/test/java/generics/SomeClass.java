240f73d2da44646304403776fe695c6f6ca14bac
Andre Winkler <gluehloch@googlemail.com>
2021-05-20 06:31:35 +0200

    Generics... a mysterious construction...

--- src/test/java/generics/SomeClass.java
+++ src/test/java/generics/SomeClass.java
@@ -1,5 +1,8 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SomeClass implements SomeInterface<SomeClass> {
 
     public SomeClass getSomeClass() {
@@ -10,4 +13,28 @@ public class SomeClass implements SomeInterface<SomeClass> {
         return SomeEnum.ONE;
     }
     
+    public void doSomething() {
+        SomeClass someClass = new SomeClass();
+        AnotherClass anotherClass = new AnotherClass();
+        
+        SomeInterface<SomeClass> someClass2 = someClass.getSomeClass();
+        SomeInterface<AnotherClass> someClass3 = anotherClass.getSomeClass();
+        
+        List<SomeInterface<?>> list = new ArrayList<SomeInterface<?>>();
+        list.add(someClass2);
+        list.add(someClass3);
+        
+        SomeInterface<?> someClass4 = list.get(0).getSomeClass();
+        // Die Typinformation (?) geht hier verloren.
+    }
+    
+    public class AnotherClass implements SomeInterface<AnotherClass> {
+
+        @Override
+        public SomeInterface<AnotherClass> getSomeClass() {
+            return this;
+        }
+        
+    }
+
 }
