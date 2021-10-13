b0558feb6cd5cd8af2b365bbfe848e63eef79fef
gluehloch <gluehloch@googlemail.com>
2021-10-13 07:21:19 +0200

    static function import...

--- src/test/java/streams/StreamExamples.java
+++ src/test/java/streams/StreamExamples.java
@@ -1,12 +1,13 @@
 package streams;
 
+import static java.util.function.Function.identity;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,7 +35,7 @@ public class StreamExamples {
     @Test
     void streamToMap() {
         List<String> list = List.of("Andre", "Lars", "Adam", "Erwin", "Christine");
-        Map<String, String> map = list.stream().collect(Collectors.toMap(i -> i, Function.identity()));
+        Map<String, String> map = list.stream().collect(Collectors.toMap(i -> i, identity()));
         assertThat(map.keySet()).containsExactlyInAnyOrder(list.toArray(new String[0]));
     }
 
