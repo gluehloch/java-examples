88a26398a132aebbe9bd7930f021e6fbd11e234a
gluehloch <gluehloch@googlemail.com>
2022-10-05 16:21:51 +0200

    Update to JDK 18

--- src/test/java/stringformat/StringFormatter.java
+++ src/test/java/stringformat/StringFormatter.java
@@ -6,22 +6,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class StringFormatter {
+class StringFormatter {
 
 	/**
 	 * Siehe auch
 	 * {@linkplain https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/Formatter.html}.
 	 */
 	@Test
-	public void printf() {
+	void printf() {
 		System.out.printf("Das ist mein %d. Test", 1);
 		assertThat(String.format("Das ist mein %d. Test.", 1)).isEqualTo("Das ist mein 1. Test.");
 		assertThat(String.format("Das ist mein %1$d. und mein %1$d. Test. %2$d", 1, 2))
-				.isEqualTo("Das ist mein 1. und mein 1. Test. 2");
+    			.isEqualTo("Das ist mein 1. und mein 1. Test. 2");
 	}
 
 	@Test
-	public void formatter() {
+	void formatter() {
 		String str = format(() -> "string {}", "string");
 		assertThat(str).isEqualTo("string string");
 	}
