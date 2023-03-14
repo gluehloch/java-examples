3f58f39709198d39ec247566afc68a396ec9d870
Andre Winkler <gluehloch@googlemail.com>
2023-03-14 14:58:48 +0100

    How to init an array.

---
+++ src/main/java/de/awtools/array/ArrayExamples.java
@@ -0,0 +1,9 @@
+package de.awtools.array;
+
+public class ArrayExamples {
+
+    public void create() {
+        String[] strings = new String[] {"Winkler", "Bicker", "Dornbusch"};
+        String[] strings2 = {"Winkler", "Bicker", "Dornbusch"};
+    }
+}
