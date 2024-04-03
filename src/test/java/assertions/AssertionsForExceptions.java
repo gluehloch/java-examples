19c0f7c832080f52aed46313f80767e44225ad16
gluehloch <gluehloch@googlemail.com>
2024-04-03 20:33:47 +0200

    Added assertj example for exception testing.

---
+++ src/test/java/assertions/AssertionsForExceptions.java
@@ -0,0 +1,20 @@
+package assertions;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
+
+class AssertionsForExceptions {
+
+    @Test
+    void validateThatException() {
+        assertThatExceptionOfType(Exception.class)
+                .isThrownBy(this::throwsAnException)
+                .withMessage("That was a failure.");
+    }
+
+    private void throwsAnException() throws Exception {
+        throw new Exception("That was a failure.");
+    }
+
+}
